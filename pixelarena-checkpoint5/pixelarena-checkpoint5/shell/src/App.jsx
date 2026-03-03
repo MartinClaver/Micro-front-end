343135bc098d7f612948233e59bf67c4fc5871cd
mclav <m.claver99@gmail.com>
2026-03-03 14:33:40 +0100

    checkpoint catalog

--- pixelarena-checkpoint5/pixelarena-checkpoint5/shell/src/App.jsx
+++ pixelarena-checkpoint5/pixelarena-checkpoint5/shell/src/App.jsx
@@ -3,6 +3,8 @@ import './App.css';
 
 const Header = lazy(() => import('mfeHeader/./Navbar'));
 const Lobby  = lazy(() => import('mfeLobby/./Lobby'));
+const Catalog = lazy(() => import('mfeCatalog/./Catalog'));
+
 // TODO: importer le Catalog depuis mfeCatalog
 
 function LoadingFallback({ name }) {
@@ -25,7 +27,9 @@ function App() {
           </section>
 
           <section className="section">
-            {/* TODO: afficher le Catalog ici avec un Suspense */}
+              <Suspense fallback={<LoadingFallback name="Catalog" />}>
+                  <Catalog />
+              </Suspense>
           </section>
         </div>
       </main>
